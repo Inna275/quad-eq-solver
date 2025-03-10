88f8bfe0c62f31baf802d85aaa9be54a81607b8b
Inna Denysiuk <innadenysiuk275@gmail.com>
2025-03-10 17:12:47 +0200

    Add console output for equation and roots

---
+++ src/resultsPrinter.js
@@ -0,0 +1,17 @@
+const showEquation = (a, b, c) => {
+  const equation = `(${a})x^2 + (${b})x + (${c}) = 0`;
+  console.log(`Equation is: ${equation}`);
+};
+
+const showRoots = (a, b, c) => {
+  const roots = solve(a, b, c);
+  const n = roots.length;
+
+  console.log(`There are ${n} roots`);
+
+  if (n === 0) return;
+
+  for (let i = 0; i < n; i++) {
+    console.log(`x${i + 1} = ${roots[i]}`);
+  }
+};
